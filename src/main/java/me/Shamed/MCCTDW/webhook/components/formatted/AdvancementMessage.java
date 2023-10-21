38d50e013eac3d65cc60359a5fc394ef7850ea68
tsrk. <tsrk@tsrk.me>
2023-10-21 20:01:34 +0200

    fix: make advancements kinda fail proof

--- src/main/java/me/Shamed/MCCTDW/webhook/components/formatted/AdvancementMessage.java
+++ src/main/java/me/Shamed/MCCTDW/webhook/components/formatted/AdvancementMessage.java
@@ -5,9 +5,11 @@ import me.Shamed.MCCTDW.webhook.components.DiscordMessage;
 import me.Shamed.MCCTDW.webhook.components.embed.DiscordEmbed;
 import me.Shamed.MCCTDW.webhook.components.embed.DiscordEmbedAttributes;
 import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementDisplay;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class AdvancementMessage extends DiscordMessage {
 
@@ -15,12 +17,17 @@ public class AdvancementMessage extends DiscordMessage {
         Player p = event.getPlayer();
 
         DiscordEmbed embed = new DiscordEmbed();
+        AdvancementDisplay display = event.getAdvancement().getDisplay();
+        String namespacedKey = event.getAdvancement().getKey().getKey();
         embed.setTitle(String.format("%s earned an achievement.", p.getName()))
-                .setDescription(LocaleProvider.translateLocale(convertNamespaceToLocale(event.getAdvancement().getKey().getKey())+".description"));
+                .setDescription(display != null ? asNonNull(display.getDescription()) :
+                        LocaleProvider.translateLocale(convertNamespaceToLocale(namespacedKey + ".description")));
 
         embed.setColor("#55FFFF");
-        embed.addField(new DiscordEmbedAttributes.DiscordEmbedField("Achievement", LocaleProvider.translateLocale(convertNamespaceToLocale(event.getAdvancement().getKey().getKey())+".title")))
-        .addField(new DiscordEmbedAttributes.DiscordEmbedField("Namespaced Key", event.getAdvancement().getKey().getKey(), true));
+        embed.addField(new DiscordEmbedAttributes.DiscordEmbedField("Achievement", display != null
+                        ? asNonNull(display.getTitle())
+                        : LocaleProvider.translateLocale(convertNamespaceToLocale(namespacedKey + ".title"))))
+        .addField(new DiscordEmbedAttributes.DiscordEmbedField("Namespaced Key", namespacedKey, true));
 
         embed.setThumbnail(new DiscordEmbedAttributes.DiscordEmbedMedia.DiscordEmbedThumbnail(String.format("https://minotar.net/helm/%s/100.png", p.getUniqueId())));
         this.addEmbed(embed);
@@ -29,5 +36,12 @@ public class AdvancementMessage extends DiscordMessage {
 
     private String convertNamespaceToLocale(String namespacedKey){
         return "advancements."+namespacedKey.replace('/','.');
+    }
+
+    @NotNull
+    private String asNonNull(@Nullable String nullableString) {
+        if (nullableString == null)
+            return "(null)";
+        return nullableString;
     }
 }
